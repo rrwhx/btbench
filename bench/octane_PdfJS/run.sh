64fda0976c014631041b1240e08482c2d4bc5672
Xinyu Li <lixinyu@loongson.cn>
2024-04-11 15:34:09 +0800

    add v8 options --single-threaded
    Signed-off-by: Xinyu Li <lixinyu@loongson.cn>

--- bench/octane_PdfJS/run.sh
+++ bench/octane_PdfJS/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 CUR_D=$(basename "$PWD")
-$CMD_PREFIX /usr/bin/node ../octane/octane.js ${CUR_D#*_} >stdout.txt 2>stderr.txt
+$CMD_PREFIX /usr/bin/node --single-threaded ../octane/octane.js ${CUR_D#*_} >stdout.txt 2>stderr.txt
 
